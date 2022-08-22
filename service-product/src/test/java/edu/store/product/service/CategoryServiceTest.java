02d36b7df40af8a7f32faf70af54aaf5e4d51de5
Julian Lafratta <julian.lafratta@gmail.com>
2022-08-21 22:44:43 -0300

    test findAll

--- service-product/src/test/java/edu/store/product/service/CategoryServiceTest.java
+++ service-product/src/test/java/edu/store/product/service/CategoryServiceTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
@@ -39,6 +42,38 @@ public class CategoryServiceTest {
         when(categoryRepository.findAll(defaultSorting)).thenReturn(categories);
 
         List<Category> categoriesResult = categoryService.findAll(null, null);
+
+        assertEquals(categories.size(), categoriesResult.size());
+        assertEquals(categories, categoriesResult);
+        verify(categoryRepository, times(1)).findAll(defaultSorting);
+    }
+
+    @Test
+    public void findAllPagedTestOk() {
+        List<Category> categories = categoryMock.getList();
+        Sort defaultSorting = Sort.by("modifyDate").descending();
+        Integer page = 0;
+        Integer size = 3;
+        PageRequest request = PageRequest.of(page, size, defaultSorting);
+        Page<Category> response = new PageImpl(categories);
+
+        when(categoryRepository.findAll(request)).thenReturn(response);
+
+        List<Category> result = categoryService.findAll(page, size);
+
+        assertEquals(categories.size(), result.size());
+        assertEquals(categories, result);
+        verify(categoryRepository, times(1)).findAll(request);
+    }
+
+    @Test
+    void findAllPagedNullTestOk() {
+        List<Category> categories = categoryMock.getList();
+        Sort defaultSorting = Sort.by("modifyDate").descending();
+
+        when(categoryRepository.findAll(defaultSorting)).thenReturn(categories);
+
+        List<Category> categoriesResult = categoryService.findAll(0, null);
 
         assertEquals(categories.size(), categoriesResult.size());
         assertEquals(categories, categoriesResult);
