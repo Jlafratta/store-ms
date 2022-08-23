306ea263f7016fffd2ae0fae6de4e46e65e9fa33
Julian Lafratta <julian.lafratta@gmail.com>
2022-08-23 01:22:45 -0300

    save, delete and enable/disable operations tests

--- service-product/src/test/java/edu/store/product/service/CategoryServiceTest.java
+++ service-product/src/test/java/edu/store/product/service/CategoryServiceTest.java
@@ -14,7 +14,7 @@ import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -67,7 +67,7 @@ public class CategoryServiceTest {
     }
 
     @Test
-    void findAllPagedNullTestOk() {
+    public void findAllPagedNullTestOk() {
         List<Category> categories = categoryMock.getList();
         Sort defaultSorting = Sort.by("modifyDate").descending();
 
@@ -78,5 +78,69 @@ public class CategoryServiceTest {
         assertEquals(categories.size(), categoriesResult.size());
         assertEquals(categories, categoriesResult);
         verify(categoryRepository, times(1)).findAll(defaultSorting);
+    }
+
+    @Test
+    public void saveTestOk() {
+        Category toSave = new Category();
+        Category saved = categoryMock.getOne();
+        toSave.setName(saved.getName());
+        saved.setModifyDate(saved.getCreateDate());
+
+        when(categoryRepository.save(toSave)).thenReturn(saved);
+
+        Category result = categoryService.save(toSave);
+
+        assertEquals(result.getId(), saved.getId());
+        assertNotNull(result.getCreateDate());
+        assertNotNull(result.getModifyDate());
+
+        verify(categoryRepository, times(1)).save(toSave);
+    }
+
+    @Test
+    public void deleteTestOk() {
+        Long id = 1L;
+        categoryService.delete(id);
+        boolean result = categoryService.existsById(id);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void toggleEnabledTestOk() {
+        Category category = categoryMock.getOne();
+
+        when(categoryRepository.getById(category.getId())).thenReturn(category);
+
+        category.setEnabled(!category.isEnabled());
+
+        when(categoryService.save(category)).thenReturn(category);
+
+        Category result = categoryService.toggleEnabled(category.getId());
+
+        assertEquals(category.getId(), result.getId());
+        assertTrue(result.isEnabled());
+
+        verify(categoryRepository, times(1)).getById(category.getId());
+    }
+
+    @Test
+    public void toggleDisabledTestOk() {
+        Category category = categoryMock.getOne();
+        category.setEnabled(false);
+
+        when(categoryRepository.getById(category.getId())).thenReturn(category);
+
+        category.setEnabled(!category.isEnabled());
+
+        when(categoryService.save(category)).thenReturn(category);
+
+        Category result = categoryService.toggleEnabled(category.getId());
+
+        assertEquals(category.getId(), result.getId());
+        assertFalse(result.isEnabled());
+
+        verify(categoryRepository, times(1)).getById(category.getId());
     }
 }
