2c76aa2413df35baac3023def2efaf7de728b072
Julian Lafratta <52461452+Jlafratta@users.noreply.github.com>
2022-08-24 00:19:23 -0300

    findById with custom exceptions added (#2)

--- service-product/src/main/java/edu/store/product/service/CategoryService.java
+++ service-product/src/main/java/edu/store/product/service/CategoryService.java
@@ -1,6 +1,7 @@
 package edu.store.product.service;
 
 import edu.store.product.domain.model.Category;
+import edu.store.product.exception.EntityNotFoundException;
 import edu.store.product.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,11 @@ public class CategoryService extends GenericService<Category, Long>{
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
         repository = categoryRepository;
+    }
+
+    @Override
+    public Category findById(Long id) {
+        return super.findByIdOptional(id)
+                .orElseThrow(() -> new EntityNotFoundException(Category.class.getSimpleName(), id.toString()));
     }
 }
--- service-product/src/main/java/edu/store/product/service/GenericService.java
+++ service-product/src/main/java/edu/store/product/service/GenericService.java
@@ -1,20 +1,31 @@
 package edu.store.product.service;
 
 import edu.store.product.domain.model.BaseEntity;
+import edu.store.product.exception.EntityNotFoundException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Objects.isNull;
 
-public class GenericService<T extends BaseEntity, ID> {
+public abstract class GenericService<T extends BaseEntity, ID> {
 
     protected JpaRepository<T, ID> repository;
 
     public T save(T entity) {
         return repository.save(entity);
+    }
+
+    public T findById(ID id) {
+        return findByIdOptional(id)
+                .orElseThrow(() -> new EntityNotFoundException(id.toString()));
+    }
+
+    public Optional<T> findByIdOptional(ID id) {
+        return repository.findById(id);
     }
 
     public List<T> findAll(Integer page, Integer size) {
@@ -23,11 +34,22 @@ public class GenericService<T extends BaseEntity, ID> {
                 repository.findAll(defaultSorting());
     }
 
+    public T update(T entity) {
+        checkExists((ID) entity.getId());
+        return save(entity);
+    }
+
     public void delete(ID id) {
+        checkExists(id);
         repository.deleteById(id);
     }
 
+    private void checkExists(ID id) {
+        if(!repository.existsById(id)) throw new EntityNotFoundException(id.toString());
+    }
+
     public T toggleEnabled(ID id) {
+        checkExists(id);
         T entity = repository.getById(id);
         entity.setEnabled(!entity.isEnabled());
         return save(entity);
