b67895fb66d5d632cf34038ba1e6ede8ea52a007
Julian Lafratta <julian.lafratta@gmail.com>
2022-09-01 00:31:15 -0300

    removed unused imports

--- client-service/src/main/java/edu/store/client/domain/model/Person.java
+++ client-service/src/main/java/edu/store/client/domain/model/Person.java
@@ -1,16 +1,13 @@
 package edu.store.client.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 import java.time.LocalDate;
 
 @AllArgsConstructor
